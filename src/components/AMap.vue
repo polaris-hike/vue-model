d3b104069b2b61cab949724f27553564bd878bd1
wuxuweilalala <2509165479@qq.com>
2020-12-17 09:39:15 +0800

    地图

--- src/components/AMap.vue
+++ src/components/AMap.vue
@@ -20,15 +20,6 @@
                 查看详情
             </div>
         </div>
-
-        <!--<v-chart
-                style="width: 100vw; height: 100vh"
-                class="map"
-                id="echarts-amap"
-                autoresize
-                ref="instance"
-                :options="options"
-        ></v-chart>-->
     </div>
     <!--<el-amap vid="amapDemo" :zoom="zoom" :center="center">
     </el-amap>-->
@@ -40,65 +31,7 @@
     import vECharts from "vue-echarts";
     import "echarts-extension-amap";
     import AMapLoader from '@amap/amap-jsapi-loader';
-
     import {convertData, data} from "./mapPoint";
-
-    const shenzhen = [
-        {
-            name: "宝安区",
-            value: [113.890267, 22.557764, 270],
-        },
-        {
-            name: "龙岗区",
-            value: [114.24771, 22.71986, 270],
-        },
-        {
-            name: "南山区",
-            value: [113.93029, 22.53291, 270],
-        },
-        {
-            name: "福田区",
-            value: [114.05571, 22.52245, 270],
-        },
-    ];
-    const shenzhen1 = [
-        {
-            id: "0",
-            lnglat: [113.890267, 22.557764],
-        },
-        {
-            id: "2",
-            lnglat: [114.24771, 22.71986],
-        },
-        {
-            id: "3",
-            lnglat: [113.93029, 22.53291],
-        },
-        {
-            id: "0",
-            lnglat: [114.05571, 22.52245],
-        },
-        {
-            id:'1',
-            lnglat: [113.52,22.3]
-        },
-        {
-            id:'0',
-            lnglat: [113.889342,22.561878]
-        },
-        {
-            id:'1',
-            lnglat: [113.889764,22.562328]
-        },
-        {
-            id:'0',
-            lnglat: [113.52,22.3]
-        },{
-            id:'1',
-            lnglat: [113.52,22.3]
-        }
-
-    ];
     const img = {
         error: require("@/assets/index/error.png"),
         warn: require("@/assets/index/warn.png"),
@@ -169,7 +102,7 @@
 
                             coordinateSystem: "amap",
 
-                            data: shenzhen,
+                            data: [],
                             symbolSize: function (val) {
                                 return val[2] / 10;
                             },
@@ -212,8 +145,8 @@
                 this.$router.push('/device')
             },
             handleMapClick(zoom, center) {
-                this.$store.commit('setZoom', zoom)
-                this.isSecondShow = zoom >= 15;
+              this.$store.commit('setZoom', zoom)
+              this.isSecondShow = zoom >= 15;
             },
             init(AMap) {
 
@@ -235,29 +168,22 @@
                 this.mapInstance.on('zoomend', (e) => {
                     this.handleMapClick(this.mapInstance.getZoom(), this.mapInstance.getCenter())
                 })
-                /*this.mapInstance.on('mouseover', (e) => {
-                    console.log('mouseover');
-                    console.log(e);
-                })
-                this.mapInstance.on('mouseout', (e) => {
-                    console.log('mouseout');
-                    console.log(e);
-                })*/
+              this.mapInstance.on('click', (e) => {
+                if(!e.clusterData) {
+                  this.boxShow = false;
+                }
+              })
                 this.$get('/api/v1/map').then(res=>{
                   res.data.base.forEach(item=>{
                     item.lnglat = item.lnglat.map(i=>{
                       if(i){return Number(i)}
                     })
                   })
-                  res.data.base.push( {
-                    id:'0',
-                    lnglat: [113.889342,22.561878]
-                  },)
                     const mapPoint = res.data.base
-                  const gridSize = 60
-                    var count = mapPoint.length;
+                    const gridSize = 60
+                    const count = mapPoint.length;
 
-                    var _renderClusterMarker = function (context) {
+                  const _renderClusterMarker = function (context) {
                         var factor = Math.pow(context.count / count, 1 / 18);
                         var div = document.createElement('div');
                         div.style.boxSizing='border-box';
@@ -280,20 +206,20 @@
                         context.marker.setOffset(new AMap.Pixel(-size / 2, -size / 2));
                         context.marker.setContent(div)
                     };
-                    var _renderMarker = function (context) {
+                  const _renderMarker = function (context) {
                         var content
-                      if (context.data[0].isonline == '0') {
-                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/normal.png');background-size:100% 100%;  height: 36px; width: 30px;"></div>`;
+                      if (context.data[0].isonline === 0) {
+                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/normal1.png');background-size:100% 100%;  height: 36px; width: 30px;"></div>`;
                             // content = '<div style="background-color: hsla(180, 100%, 50%, 0.3); height: 18px; width: 18px; border: 1px solid hsl(180, 100%, 40%); border-radius: 12px; box-shadow: hsl(180, 100%, 50%) 0px 0px 3px;"></div>';
                         }
-                        if (context.data[0].isonline == '1') {
+                        if (context.data[0].isonline === 1) {
+                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/warn.png');background-size:100% 100%;  height: 36px; width: 30px;"></div>`;
+                        }
+                        if (context.data[0].isonline === 2) {
+                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/stop.png');background-size:100% 100%; height: 36px; width: 30px;"></div>`;
+                        }
+                        if (context.data[0].isonline === 3) {
                             content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/error.png');background-size:100% 100%;  height: 36px; width: 30px;"></div>`;
-                        }
-                        if (context.data[0].isonline == '2') {
-                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/warn.png');background-size:100% 100%; height: 36px; width: 30px;"></div>`;
-                        }
-                        if (context.data[0].isonline == '3') {
-                            content = `<div style="background-image: url('https://wedge.oss-cn-shenzhen.aliyuncs.com/static/icon/stop.png');background-size:100% 100%;  height: 36px; width: 30px;"></div>`;
                         }
 
                         var offset = new AMap.Pixel(-9, -9);
@@ -311,17 +237,17 @@
                     });
                     cluster.on('click',(e)=>{
                         if(this.isMobile) return
-                        if(e.clusterData[0].isonline == '0'){
+                      if(e.clusterData[0].isonline === 0){
                             this.currentBoxType = 'normal'
                         }
-                        if(e.clusterData[0].isonline == '1'){
-                            this.currentBoxType = 'error'
-                        }
-                        if(e.clusterData[0].isonline == '2'){
+                        if(e.clusterData[0].isonline === 1){
                             this.currentBoxType = 'warn'
                         }
-                        if(e.clusterData[0].isonline == '3'){
+                        if(e.clusterData[0].isonline === 2){
                             this.currentBoxType = 'stop'
+                        }
+                        if(e.clusterData[0].isonline === 3){
+                            this.currentBoxType = 'error'
                         }
                         this.boxShow = true;
                         const left = Number(e.marker.dom.style.left.split('px')[0])
@@ -416,9 +342,9 @@
             border-bottom: 1px solid #e54e4e;
         }
         &.warn {
-            box-shadow: #db4b4b 0 0 8px inset;
-            border-top: 1px solid #db4b4b;
-            border-bottom: 1px solid #db4b4b;
+            box-shadow: #e99002 0 0 8px inset;
+            border-top: 1px solid #e99002;
+            border-bottom: 1px solid #e99002;
         }
         &.stop {
             box-shadow: #999999 0 0 8px inset;
